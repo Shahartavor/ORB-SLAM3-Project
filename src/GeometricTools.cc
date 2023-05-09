5bd1786cc0f5d2018f1b80dd9613a408088c878a
yaelN21 <yael1234n@gmail.com>
2023-05-09 17:09:40 +0300

    use poly to triangulation - comments for strat

--- src/GeometricTools.cc
+++ src/GeometricTools.cc
@@ -83,4 +83,66 @@ bool GeometricTools::Triangulate(Eigen::Vector3f &x_c1, Eigen::Vector3f &x_c2,Ei
     return true;
 }
 
+    bool GeometricTools::TriangulatePoly(Eigen::Vector3f &x_c1, Eigen::Vector3f &x_c2,Eigen::Matrix<float,3,4> &Tc1w ,Eigen::Matrix<float,3,4> &Tc2w , Eigen::Vector3f &x3D) {
+
+        //We got a match of 2 points from triangulation - x_c1 and x_c2.
+        //u = x_c1 is point1 =(y,x) , u' = x_c2 is point2 =(y',x')
+        //Tc1w is camera matrix1= p, Tc2w is camera matrix2= p'
+
+        // params is polynomial 6th degrees
+        //1.calculate 2 epoipolar lines: e =essentialMatrix*x
+        //2.function calcluate distance from point to line : x1,y1 - point , a,b,c-parameters of line
+        //    void shortest_distance(float x1, float y1, float a, float b,
+        //                           float c)
+        //    {
+        //        float d = fabs((a * x1 + b * y1 + c))
+        //                  / (sqrt(a * a + b * b));
+        //        cout << "Perpendicular distance is, " << d << endl;
+        //        return;
+        //    }
+        //3.params = distance of u from u epipole + distance of u' from u' epipole
+        //4. poly:
+        std::vector<double> Poly::PreparePolyCoeffs(const Poly::PolyParams &params) const {
+            double a, b, c, d, e, f;
+            std::tie(a, b, c, d, e, f) = params;
+            std::vector<double> result = {
+                    // a*b*d^2 - b^2*c*d
+                    a * b * d * d - b * b * c * d,
+                    // - 2*b^2*d^2*f^2*z + a^2*d^2*z - d^4*f^4*z - b^2*c^2*z - b^4*z
+                    -2 * b * b * d * d * f * f + a * a * d * d - d * d * d * d * f * f * f * f - b * b * c * c -
+                    b * b * b * b,
+                    // - 4*b^2*c*d*f^2*z^2 - 2*b^2*c*d*e^2*z^2 - 4*a*b*d^2*f^2*z^2 + 2*a*b*d^2*e^2*z^2 + a^2*c*d*z^2 - 4*c*d^3*f^4*z^2 - a*b*c^2*z^2 - 4*a*b^3*z^2
+                    -4 * b * b * c * d * f * f - 2 * b * b * c * d * e * e - 4 * a * b * d * d * f * f +
+                    2 * a * b * d * d * e * e + a * a * c * d - 4 * c * d * d * d * f * f * f * f - a * b * c * c -
+                    4 * a * b * b * b,
+                    // - 8*a*b*c*d*f^2*z^3 - 6*c^2*d^2*f^4*z^3 - 2*b^2*c^2*f^2*z^3 - 2*a^2*d^2*f^2*z^3 - 2*b^2*c^2*e^2*z^3 + 2*a^2*d^2*e^2*z^3 - 6*a^2*b^2*z^3
+                    -8 * a * b * c * d * f * f - 6 * c * c * d * d * f * f * f * f - 2 * b * b * c * c * f * f -
+                    2 * a * a * d * d * f * f - 2 * b * b * c * c * e * e + 2 * a * a * d * d * e * e -
+                    6 * a * a * b * b,
+                    // - 4*a^2*c*d*f^2*z^4 + 2*a^2*c*d*e^2*z^4 - 4*a*b*c^2*f^2*z^4 + a*b*d^2*e^4*z^4 - 2*a*b*c^2*e^2*z^4 - b^2*c*d*e^4*z^4 - 4*c^3*d*f^4*z^4 - 4*a^3*b*z^4
+                    -4 * a * a * c * d * f * f + 2 * a * a * c * d * e * e - 4 * a * b * c * c * f * f +
+                    a * b * d * d * e * e * e * e - 2 * a * b * c * c * e * e - b * b * c * d * e * e * e * e -
+                    4 * c * c * c * d * f * f * f * f - 4 * a * a * a * b,
+                    // a^2*d^2*e^4*z^5 - 2*a^2*c^2*f^2*z^5 - b^2*c^2*e^4*z^5 - c^4*f^4*z^5 - a^4*z^5
+                    a * a * d * d * e * e * e * e - 2 * a * a * c * c * f * f - b * b * c * c * e * e * e * e -
+                    c * c * c * c * f * f * f * f - a * a * a * a,
+                    // a^2*c*d*e^4*z^6 - a*b*c^2*e^4*z^6
+                    a * a * c * d * e * e * e * e - a * b * c * c * e * e * e * e
+            };
+            result.resize(FindPolynomialOrder(result) + 1);
+            double max_coeff = *std::max_element(result.begin(), result.end());
+            if (max_coeff > 0)
+            {
+                std::transform(result.begin(), result.end(), result.begin(), [&max_coeff](double& a){ return a / max_coeff; });
+            }
+            else if (max_coeff < 0)
+            {
+                double min_coeff = *std::min_element(result.begin(), result.end());
+                std::transform(result.begin(), result.end(), result.begin(), [&min_coeff](double& a){ return a / min_coeff; });
+            }
+            return result;
+        }
+    }
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 } //namespace ORB_SLAM
